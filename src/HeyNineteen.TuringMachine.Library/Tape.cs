d4e3f18cfd7f07b1247098f106f711c1888e0551
heynineteen <steve.james@heynineteen.co.uk>
2021-09-26 17:30:26 +0100

    Tape uses TwoWayList

--- src/HeyNineteen.TuringMachine.Library/Tape.cs
+++ src/HeyNineteen.TuringMachine.Library/Tape.cs
@@ -3,73 +3,46 @@ namespace HeyNineteen.TuringMachine.Library
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using Core.Collections;
 
     public class Tape
     {
-        private int _position;
-        
-        // value at position 0 is held at index 0 of _positiveIndexValues
-        private readonly List<char?> _positiveIndexValues = new() {null};
+        private readonly TwoWayList<char?> _values = new(new char?[] { null }, null);
 
-        // value at index 0 is ignored
-        private readonly List<char?> _negativeIndexValues = new() {null};
+        public List<char?> State => _values.ToList();
+
+        public int Position { get; private set; }
 
         public void MoveLeft()
         {
-            _position--;
+            Position--;
             EnsureCapacity();
         }
 
         public void MoveRight()
         {
-            _position++;
+            Position++;
             EnsureCapacity();
         }
 
         private void EnsureCapacity()
         {
-            if (_position < 0)
-            {
-                var absolutePosition = Math.Abs(_position);
-                while (absolutePosition > _negativeIndexValues.Count - 1)
-                {
-                    _negativeIndexValues.Add(null);
-                }
-            }
-            else if (_position > 0)
-            {
-                while (_position > _positiveIndexValues.Count - 1)
-                {
-                    _positiveIndexValues.Add(null);
-                }
-            }
+            if (Position < _values.LowestIndex)
+                _values.AddBack(null);
+            else if (Position > _values.HightestIndex)
+                _values.AddForward(null);
         }
 
         public void Erase()
         {
-            if (_position >= 0)
-                _positiveIndexValues[_position] = null;
-            else
-                _negativeIndexValues[Math.Abs(_position)] = null;
+            _values[Position] = null;
         }
 
         public void Print(char value)
         {
-            if (_position >= 0)
-                _positiveIndexValues[_position] = value;
-            else
-                _negativeIndexValues[Math.Abs(_position)] = value;
+            _values[Position] = value;
         }
 
-        public char? Read()
-        {
-            return _position >= 0
-                ? _positiveIndexValues[_position]
-                : _negativeIndexValues[Math.Abs(_position)];
-        }
-
-        public List<char?> State => new(_negativeIndexValues.Skip(1).Reverse().Concat(_positiveIndexValues));
-
-        public int Position => _position;
+        public char? Read() => _values[Position];
     }
 }
