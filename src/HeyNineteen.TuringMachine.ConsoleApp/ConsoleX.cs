b5ba26038113c4c6aefd3374d3fda4f1ff4b12c3
heynineteen <steve.james@heynineteen.co.uk>
2023-12-21 12:31:23 +0000

    Use AnsiEscape codes defined in Core

---
+++ src/HeyNineteen.TuringMachine.ConsoleApp/ConsoleX.cs
@@ -0,0 +1,55 @@
+﻿namespace HeyNineteen.TuringMachine.ConsoleApp;
+
+using Library;
+using System;
+using static Core.Console.AnsiEscapeCodes;
+
+public static class ConsoleX
+{
+    public static void Write(int count, CompleteConfiguration state)
+    {
+        Console.WriteLine(MetaDataText(count, state));
+        Console.WriteLine(TapeValuesText(state));
+    }
+
+    private static readonly Func<int, CompleteConfiguration, string> MetaDataText = (count, state) =>
+        $"StepCount: {count}, MConfiguration: {state.MConfiguration}";
+
+    private static readonly Func<CompleteConfiguration, string> TapeValuesText = state =>
+        TextWithIndexInCentre(state.TapeAdjustedValues, state.TapeAdjustedPosition);
+
+    public static bool IsFirstWrite = true;
+
+    public static void Overwrite(int count, CompleteConfiguration state)
+    {
+        var moveUp = !IsFirstWrite ? MoveUp(1) : null;
+        IsFirstWrite = false;
+
+        var metaDataText = MetaDataText(count, state);
+        var tapeValuesText = TapeValuesText(state);
+        Console.WriteLine($"{moveUp}{ClearLine}{MoveStartOfLine}{metaDataText}");
+        Console.Write($"{ClearLine}{MoveStartOfLine}{tapeValuesText}");
+    }
+
+    private static int HorizontalCentrePosition() => Console.BufferWidth / 2;
+
+    private static string TextWithIndexInCentre(ReadOnlySpan<char> text, int index)
+    {
+        var leftOffset = index;
+        var horizontalCentre = HorizontalCentrePosition();
+        var startPosition = horizontalCentre - leftOffset;
+        var truncateStart = Math.Max(Math.Abs(Math.Min(startPosition, 0)), 0);
+
+        var firstPart = text.Slice(truncateStart, index - truncateStart);
+        var centrePart = text[index];
+        var lastPart = text.Slice(index + 1, Math.Min(horizontalCentre - 1, text.Length - (index + 1)));
+
+        var setStartPosition = startPosition > 0 ? MoveRight(startPosition) : String.Empty;
+
+        var setupLine = $"{ClearLine}{MoveStartOfLine}{setStartPosition}";
+
+        var output = $"{setupLine}{firstPart}{SetReversed}{centrePart}{Reset}{lastPart}";
+
+        return output;
+    }
+}
--- src/HeyNineteen.TuringMachine.ConsoleApp/Program.cs
+++ src/HeyNineteen.TuringMachine.ConsoleApp/Program.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
+using static ConsoleX;
+
 
 class Program
 {
@@ -67,62 +69,5 @@ class Program
         }
 
         Console.WriteLine();
-    }
-
-    public static void Write(int count, CompleteConfiguration state)
-    {
-        Console.WriteLine(MetaDataText(count, state));
-        Console.WriteLine(TapeValuesText(state));
-    }
-
-    private static readonly Func<int, CompleteConfiguration, string> MetaDataText = (count, state) =>
-        $"StepCount: {count}, MConfiguration: {state.MConfiguration}";
-
-    private static readonly Func<CompleteConfiguration, string> TapeValuesText = state =>
-        TextWithIndexInCentre(state.TapeAdjustedValues, state.TapeAdjustedPosition);
-
-    public static bool IsFirstWrite = true;
-
-    public static void Overwrite(int count, CompleteConfiguration state)
-    {
-        var moveUp = !IsFirstWrite ? MoveUp(1) : null;
-        IsFirstWrite = false;
-
-        var metaDataText = MetaDataText(count, state);
-        var tapeValuesText = TapeValuesText(state);
-        Console.WriteLine($"{moveUp}{ClearLine}{MoveStartLine}{metaDataText}");
-        Console.Write($"{ClearLine}{MoveStartLine}{tapeValuesText}");
-    }
-
-    private static int HorizontalCentrePosition() => Console.BufferWidth / 2;
-
-    private const string Escape = "\u001b";
-    private const string ClearLine = $"{Escape}[2K";
-    private static readonly Func<int, string> MoveUp = positions => $"{Escape}[{positions}A";
-    private static readonly Func<int, string> MoveDown = positions => $"{Escape}[{positions}B";
-    private static readonly Func<int, string> MoveRight = positions => $"{Escape}[{positions}C";
-    private static readonly Func<int, string> MoveLeft = positions => $"{Escape}[{positions}D";
-    private static readonly string MoveStartLine = MoveLeft(1000);
-    private const string Reversed = $"{Escape}[7m";
-    private const string Reset = $"{Escape}[0m";
-
-    private static string TextWithIndexInCentre(ReadOnlySpan<char> text, int index)
-    {
-        var leftOffset = index;
-        var horizontalCentre = HorizontalCentrePosition();
-        var startPosition = horizontalCentre - leftOffset;
-        var truncateStart = Math.Max(Math.Abs(Math.Min(startPosition, 0)), 0);
-
-        var firstPart = text.Slice(truncateStart, index - truncateStart);
-        var centrePart = text[index];
-        var lastPart = text.Slice(index + 1, Math.Min(horizontalCentre - 1, text.Length - (index + 1)));
-
-        var setStartPosition = startPosition > 0 ? MoveRight(startPosition) : String.Empty;
-
-        var setupLine = $"{ClearLine}{MoveStartLine}{setStartPosition}";
-
-        var output = $"{setupLine}{firstPart}{Reversed}{centrePart}{Reset}{lastPart}";
-
-        return output;
     }
 }
